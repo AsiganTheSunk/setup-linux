6a9825e57503c305fb70d4d035b80f72bfb00ef7
Pedro Arias Ruiz <asiganthesunk@gmail.com>
2026-08-09 01:28:18 +0200

    Update setup-linux.sh
    Fix missing space

--- setup-linux.sh
+++ setup-linux.sh
@@ -36,7 +36,7 @@ alias bat="batcat"
 alias ls="eza"
 alias ll="eza -l --icons --hyperlink --git --git-repos"
 alias la="eza -la --icons --hyperlink --git --git-repos"
-alias lt="eza --tree--icons --hyperlink --git --git-repos"
+alias lt="eza --tree --icons --hyperlink --git --git-repos"
 alias ltt="eza -la --tree --level=2 --icons --hyperlink --git --git-repos"
 alias lttt="eza -la --tree --level=3 --icons --hyperlink --git --git-repos"
 EOF
